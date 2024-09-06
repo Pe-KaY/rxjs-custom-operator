59d573785a9d982d14be8106ed07dc0d2c16a17f
Pe-KaY <peekaylink@gmail.com>
2024-09-06 10:29:11 +0000

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,10 +21,15 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('rxjs-custom-operator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rxjs-custom-operator');
+  it('should multiply the numbers by a given factor', () => {
+    const app = new AppComponent();
+    const mockNumbers$ = of(1, 2, 3);
+    const multiplyBy5 = app.multiplyBy(5);
+    let output: number[] = [];
+    const result$ = multiplyBy5(mockNumbers$);
+    result$.subscribe((result) => {
+      output = [...output, result];
+    });
+    expect(output).toEqual([5, 10, 15]);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, map, of, tap,from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'rxjs-custom-operator';
+
+    valueBefore: number[] = []
+    valueAfter: number[] = []
+
+  // observable to generate random numbers
+  numbers2$ = from(Array.from({ length: 10 }, () => Math.floor(Math.random() * 11)));
+
+  ngOnInit() {
+    this.numbers2$.pipe(this.multiplyBy(10)).subscribe((result) => {
+      console.log(`value After: ${result}`);
+      this.valueAfter = [...this.valueAfter, result];
+    });
+  }
+
+  //  cusom operator to multiply the numbers by a given value
+  multiplyBy(factor: number) {
+    return (source$: Observable<number>) => {
+      return source$.pipe(
+        tap((value) => this.valueBefore = [...this.valueBefore, value]),
+        map((value) => value * factor)
+      );
+    };
+  }
 }
+
